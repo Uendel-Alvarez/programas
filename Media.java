ff05c41feef3bb3db4ea79640b92f7cc6285a745
Uendel-Alvarez <68794514+Uendel-Alvarez@users.noreply.github.com>
2020-07-25 20:49:40 -0300

    Create Media.java

---
+++ Media.java
@@ -0,0 +1,8 @@
+public class Media{
+    public static void main(String[] args){
+        double media, nota1=5.0, nota2=3.5, nota3=9.5;
+        media = (nota1+nota2+nota3)/3;
+        System.out.println("A média do aluno foi:  "+media );
+
+    }
+}
