7b6734b421d4091b3492bf404e9d085e849f27f8
Uendel-Alvarez <68794514+Uendel-Alvarez@users.noreply.github.com>
2020-08-06 10:31:33 -0300

    Create testeinteiros.php
    teste no servidor

---
+++ testeinteiros.php
@@ -0,0 +1,13 @@
+<?php
+header("Content-type:text/html;charset=utf8");
+$joao=19;
+$pedro=16;
+$jessica=17;
+$carlos=20;
+
+echo("João tem $joao anos.<br>");
+echo("Pedro tem $pedro anos.<br>");
+echo("Jéssica tem $jessica anos.<br>");
+echo("Carlos tem $carlos anos.<br>");
+
+?>
