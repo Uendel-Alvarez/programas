4776e1e425aadb460129af46ed1bb69a9a4387a6
Uendel-Alvarez <68794514+Uendel-Alvarez@users.noreply.github.com>
2021-06-12 21:28:29 -0300

    votação
    programa em C para sistema de votação

---
+++ main-votacao.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int op, totalVotos;
+	
+	totalVotos = 0;
+
+	printf(" 1- Votar\n 2- Apuracao\n 3- Sair\n");
+	printf("Informar a opcao desejada:\n");
+	scanf("%d", &op);
+
+
+
+	return 0;
+}
